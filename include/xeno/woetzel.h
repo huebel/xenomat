b9750c680e8b2c77088f96bd9c3ca8c015bfd100
Peter Hübel <huebel@strxtrs.net>
2020-03-01 14:10:16 +0100

    Use std::allocator::construct in 'apply' functions.

--- include/xeno/woetzel.h
+++ include/xeno/woetzel.h
@@ -29,31 +29,30 @@ struct io_object : woetzel {
 	template<typename ObjectType>
 	inline ObjectType& apply(ObjectType& object)
 	{
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wuninitialized"
-		return *new (&object) ObjectType(static_cast<this_io_t&>(*this));
-#pragma GCC diagnostic pop
+		std::allocator<ObjectType>().construct(
+			&object, static_cast<this_io_t&>(*this)
+		);
+		return object;
 	}
 
 	template<typename ObjectType>
 	inline const ObjectType& apply(const ObjectType& object)
 	{
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wuninitialized"
-		return *new (const_cast<ObjectType*>(&object)) ObjectType(static_cast<this_io_t&>(*this));
-#pragma GCC diagnostic pop
+		std::allocator<ObjectType>().construct(
+			&const_cast<ObjectType&>(object),
+			static_cast<this_io_t&>(*this)
+		);
+		return object;
 	}
 
 	template<typename ObjectType>
 	inline ObjectType* apply(ObjectType* pointer)
 	{
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wuninitialized"
-		return new (pointer) ObjectType(static_cast<IO&>(*this));
-#pragma GCC diagnostic pop
+		std::allocator<ObjectType>().construct(
+			pointer, static_cast<this_io_t&>(*this)
+		);
+		return pointer;
 	}
-
-//	void back() {}
 
 	template <typename Container>
 	inline Container io_std(Container c)
@@ -72,11 +71,11 @@ template <typename ENUM, typename TEXT = std::string>
 struct io_enum_traits {
 	typedef TEXT Text;
 	typedef ENUM Enum;
-	static inline const Text enum_to_string(const Enum& e) {
-		return boost::lexical_cast<TEXT>(static_cast<int>(e));
+	static inline const Text enum_to_string(const Enum e) {
+		return boost::lexical_cast<Text>(static_cast<int>(e));
 	}
-	static inline bool enum_to_string(const Enum& e, Text& text) {
-		return text = boost::lexical_cast<TEXT>(static_cast<int>(e)), true;
+	static inline bool enum_to_string(const Enum e, Text& text) {
+		return text = boost::lexical_cast<Text>(static_cast<int>(e)), true;
 	}
 	static inline const Enum string_to_enum(const Text& s) {
 		return static_cast<Enum>(boost::lexical_cast<int>(s));
@@ -86,23 +85,24 @@ struct io_enum_traits {
 #define IO_CLASS(T) template<typename IO,\
 		typename = typename std::enable_if<std::is_base_of<xeno::woetzel,IO>::value,IO>::type>\
 		T(IO io)
-#define IO_CLASS_IMPL(T) template<typename IO> T::T(IO& io)
+#define IO_CLASS_IMPL(T) template<typename IO, typename WOETZEL_IO_DEFAULT>\
+		T::T(IO io)
 
 #define IO_BASE(B) 		    B(io)
 
 
-#define IO_ATTR(m)          m(io.io_attr(#m,m))
-#define IO_ATTR_NUL(m)      m(io.io_attr_nul(#m,m))
-#define IO_ATTR_DEF(m,val)  m(io.io_attr_def(#m,m,val))
-#define IO_ATTR_OPT(m)      m(io.io_attr_def(#m,m,m))
+#define IO_ATTR(m)          m(io.io_attr(#m,*(&m)))
+#define IO_ATTR_NUL(m)      m(io.io_attr_nul(#m,*(&m)))
+#define IO_ATTR_DEF(m,val)  m(io.io_attr_def(#m,*(&m),val))
+#define IO_ATTR_OPT(m)      m(io.io_attr_def(#m,*(&m),*(&m)))
 // For IO_ENUM you can make a specialisation of the io_enum_traits template above
-#define IO_ENUM(m)          m(io.io_enum(#m,m))
-#define IO_ENUM_TEXT(m)     m(io.io_enum_text(#m,m))
-#define IO_TEXT(m)          m(io.io_text(#m,m))
-#define IO_INNER(m)         m(io.io_text(m))
-#define IO_INNER_NUL(m)     m(io.io_text_nul(m))
-#define IO_INNER_DEF(m,val) m(io.io_text_def(m,val))
-#define IO_LINK(m)          m(io.io_link(#m,m))
+#define IO_ENUM(m)          m(io.io_enum(#m,*(&m)))
+#define IO_ENUM_TEXT(m)     m(io.io_enum_text(#m,*(&m)))
+#define IO_TEXT(m)          m(io.io_text(#m,*(&m)))
+#define IO_INNER(m)         m(io.io_text(*(&m)))
+#define IO_INNER_NUL(m)     m(io.io_text_nul(*(&m)))
+#define IO_INNER_DEF(m,val) m(io.io_text_def(*(&m),val))
+#define IO_LINK(m)          m(io.io_link(#m,*(&m)))
 #define IO_PART(m)          m(io.io_part(#m))
 #define IO_LIST(e,c)        c(std::move(io.io_list(#e,#c,c)))
 #define IO_ANON_LIST(c)  	c(std::move(io.io_list(#c,nullptr,c)))
