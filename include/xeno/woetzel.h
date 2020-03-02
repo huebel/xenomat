a031e6a7b2f86ec7ac5c4e1842e7716cce2f31b8
Peter Hübel <huebel@strxtrs.net>
2020-03-02 19:54:12 +0100

    Let IO_CLASS have a reference to the io_object it's working on, io_objects should not be copied.

--- include/xeno/woetzel.h
+++ include/xeno/woetzel.h
@@ -84,32 +84,32 @@ struct io_enum_traits {
 
 #define IO_CLASS(T) template<typename IO,\
 		typename = typename std::enable_if<std::is_base_of<xeno::woetzel,IO>::value,IO>::type>\
-		T(IO io)
+		T(const IO& io)
 #define IO_CLASS_IMPL(T) template<typename IO, typename WOETZEL_IO_DEFAULT>\
-		T::T(IO io)
+		T::T(const IO& io)
 
 #define IO_BASE(B) 		    B(io)
 
 
-#define IO_ATTR(m)          m(io.io_attr(#m,*(&m)))
-#define IO_ATTR_NUL(m)      m(io.io_attr_nul(#m,*(&m)))
-#define IO_ATTR_DEF(m,val)  m(io.io_attr_def(#m,*(&m),val))
-#define IO_ATTR_OPT(m)      m(io.io_attr_def(#m,*(&m),*(&m)))
+#define IO_ATTR(m)          m(const_cast<IO&>(io).io_attr(#m,*(&m)))
+#define IO_ATTR_NUL(m)      m(const_cast<IO&>(io).io_attr_nul(#m,*(&m)))
+#define IO_ATTR_DEF(m,val)  m(const_cast<IO&>(io).io_attr_def(#m,*(&m),val))
+#define IO_ATTR_OPT(m)      m(const_cast<IO&>(io).io_attr_def(#m,*(&m),*(&m)))
 // For IO_ENUM you can make a specialisation of the io_enum_traits template above
-#define IO_ENUM(m)          m(io.io_enum(#m,*(&m)))
-#define IO_ENUM_TEXT(m)     m(io.io_enum_text(#m,*(&m)))
-#define IO_TEXT(m)          m(io.io_text(#m,*(&m)))
-#define IO_INNER(m)         m(io.io_text(*(&m)))
-#define IO_INNER_NUL(m)     m(io.io_text_nul(*(&m)))
-#define IO_INNER_DEF(m,val) m(io.io_text_def(*(&m),val))
-#define IO_LINK(m)          m(io.io_link(#m,*(&m)))
-#define IO_PART(m)          m(io.io_part(#m))
-#define IO_LIST(e,c)        c(std::move(io.io_list(#e,#c,c)))
-#define IO_ANON_LIST(c)  	c(std::move(io.io_list(#c,nullptr,c)))
+#define IO_ENUM(m)          m(const_cast<IO&>(io).io_enum(#m,*(&m)))
+#define IO_ENUM_TEXT(m)     m(const_cast<IO&>(io).io_enum_text(#m,*(&m)))
+#define IO_TEXT(m)          m(const_cast<IO&>(io).io_text(#m,*(&m)))
+#define IO_INNER(m)         m(const_cast<IO&>(io).io_text(*(&m)))
+#define IO_INNER_NUL(m)     m(const_cast<IO&>(io).io_text_nul(*(&m)))
+#define IO_INNER_DEF(m,val) m(const_cast<IO&>(io).io_text_def(*(&m),val))
+#define IO_LINK(m)          m(const_cast<IO&>(io).io_link(#m,*(&m)))
+#define IO_PART(m)          m(const_cast<IO&>(io).io_part(#m))
+#define IO_LIST(e,c)        c(std::move(const_cast<IO&>(io).io_list(#e,#c,c)))
+#define IO_ANON_LIST(c)  	c(std::move(const_cast<IO&>(io).io_list(#c,nullptr,c)))
 
 #define IO_INIT {
 
-#define IO_ARRAY(e,c)       io.io_list(#e, #c, &c[0], c+(sizeof(c)/sizeof(c[0])));
+#define IO_ARRAY(e,c)       const_cast<IO&>(io).io_list(#e, #c, &c[0], c+(sizeof(c)/sizeof(c[0])));
 
 #define IO_END }
 
